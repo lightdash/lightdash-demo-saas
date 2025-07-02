bad7d9cc1cb70b2fa1bb7c7af19e7391f4e12d8f
Jess Hitchcock <jessica.kaari.hitchcock@gmail.com>
2025-07-02 17:38:29 +0100

    added macro for date function to enable multiple warehouses

--- models/fanouts_examples/fanouts_sales_targets.sql
+++ models/fanouts_examples/fanouts_sales_targets.sql
@@ -2,7 +2,7 @@ SELECT
     target_type,
     target_value,
     quarter_start_date,
-    DATE_ADD(DATE_ADD(quarter_start_date, INTERVAL 3 MONTH), INTERVAL -1 DAY) AS quarter_end_date,
+    {{ quarter_end_date('quarter_start_date') }} AS quarter_end_date,
     target_deals,
     target_amount
 FROM {{ ref('sales_targets_raw') }}
