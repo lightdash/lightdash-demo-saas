df7d7ba4d2af0bc72f06e87968949a8641a6bb53
Jess Hitchcock <jessica.kaari.hitchcock@gmail.com>
2025-07-21 17:41:54 +0100

    added metric calc examples

--- models/demo/accounts.sql
+++ models/demo/accounts.sql
@@ -1,8 +1,22 @@
-
+with total_deal_value as ( 
+    select 
+        account_id,
+        sum(case when stage = 'Won' then amount else 0 end) as total_won_amount
+    from 
+        {{ ref('deals_raw') }}
+    group by 
+        account_id
+)
 select
-    account_id,
-    account_name,
-    industry,
-    segment
+    a.account_id,
+    a.account_name,
+    a.industry,
+    segment, 
+    tdv.total_won_amount
 from 
-    {{ ref('accounts_raw') }}
+    {{ ref('accounts_raw') }} as a 
+left join 
+    total_deal_value as tdv
+on 
+    a.account_id = tdv.account_id
+
--- models/demo/deals.sql
+++ models/demo/deals.sql
@@ -6,6 +6,7 @@ select
     plan,
     seats,
     amount,
+    sum(case when stage = 'won' then amount else 0 end) over (partition by account_id) as total_won_amount_per_account,
     -- this code keeps the date fields relevant for the demo environment
     -- it adds the difference between the current date and a fixed date (2025-01-01)
     -- to the original date fields, effectively shifting them into the future
