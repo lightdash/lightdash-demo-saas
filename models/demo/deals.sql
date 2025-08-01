d871e62e83a9523d78f761231572ed6cef7a0c53
Jess Hitchcock <jessica.kaari.hitchcock@gmail.com>
2025-08-01 11:09:25 +0100

    added multiwarehouse handling

--- models/demo/deals.sql
+++ models/demo/deals.sql
@@ -10,6 +10,6 @@ select
     -- this code keeps the date fields relevant for the demo environment
     -- it adds the difference between the current date and a fixed date (2025-01-01)
     -- to the original date fields, effectively shifting them into the future
-    {{ date_add_cross_db('created_date', 'date_diff(current_date(), \'2025-01-01\', day)') }} as created_date
+    {{ date_add_cross_db('created_date', date_diff_cross_db('current_date()', '\'2025-01-01\'', 'day')) }} as created_date
 from 
     {{ ref('deals_raw') }}
