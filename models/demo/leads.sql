0186f38a7cbd8095fd84f5b8537ec8f14380b501
Tori Whaley <torimwhaley@abbey.bac.edu>
2025-10-21 16:17:34 -0400

    casting timestamps to account for stale data

--- models/demo/leads.sql
+++ models/demo/leads.sql
@@ -7,8 +7,8 @@ WITH source_data AS ( SELECT * FROM {{ ref ('marketing_leads') }}
     CAST(lead_id AS STRING) AS lead_id   -- primary key
     , user_id                            -- foriegn key to users table
     , deal_id                            -- foriegn key to deals table
-    , created_at 
-    , CAST(converted_at AS TIMESTAMP) AS converted_at
+    , TIMESTAMP_ADD(CAST(created_at AS TIMESTAMP), INTERVAL DATE_DIFF(CURRENT_DATE(), DATE '2025-10-10', DAY) DAY) AS created_at      -- to take care of the static dates in the demo data
+    , TIMESTAMP_ADD(CAST(converted_at AS TIMESTAMP), INTERVAL DATE_DIFF(CURRENT_DATE(), DATE '2025-10-10', DAY) DAY) AS converted_at
     , lead_source
     , campaign_name
     , utm_medium
