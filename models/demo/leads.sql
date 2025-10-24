b4f63a714f4dbe771f0a7676380aaa3373efc1a8
Tori Whaley <torimwhaley@abbey.bac.edu>
2025-10-24 16:17:18 -0400

    making leads model compatible with Jess's cross-DB date-add macro

--- models/demo/leads.sql
+++ models/demo/leads.sql
@@ -7,8 +7,8 @@ WITH source_data AS ( SELECT * FROM {{ ref ('marketing_leads') }}
     CAST(lead_id AS STRING) AS lead_id   -- primary key
     , user_id                            -- foriegn key to users table
     , deal_id                            -- foriegn key to deals table
-    , TIMESTAMP_ADD(CAST(created_at AS TIMESTAMP), INTERVAL DATE_DIFF(CURRENT_DATE(), DATE '2025-10-10', DAY) DAY) AS created_at      -- to take care of the static dates in the demo data
-    , TIMESTAMP_ADD(CAST(converted_at AS TIMESTAMP), INTERVAL DATE_DIFF(CURRENT_DATE(), DATE '2025-10-10', DAY) DAY) AS converted_at
+    , {{ date_add_cross_db('CAST(created_at AS TIMESTAMP)', date_diff_cross_db('current_date()', '\'2025-10-10\'', 'day')) }} as created_at
+    , {{ date_add_cross_db('CAST(converted_at AS TIMESTAMP)', date_diff_cross_db('current_date()', '\'2025-10-10\'', 'day')) }} as converted_at  -- to take care of the static dates in the demo data
     , lead_source
     , campaign_name
     , utm_medium
