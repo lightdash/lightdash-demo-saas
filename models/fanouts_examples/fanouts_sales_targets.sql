d8589a89cf9ea812251f0c5f9c6a2846e4b77cb5
Jess Hitchcock <jess@lightdash.com>
2026-05-25 13:08:14 +0100

    removed fanout examples from the SaaS demo

--- models/fanouts_examples/fanouts_sales_targets.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    target_type,
-    target_value,
-    quarter_start_date,
-    {{ quarter_end_date('quarter_start_date') }} AS quarter_end_date,
-    target_deals,
-    target_amount
-FROM {{ ref('sales_targets_raw') }}
-   
